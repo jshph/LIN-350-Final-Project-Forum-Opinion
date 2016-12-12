aefb4670f60e8032ffe3a0a44996849c68ad6eb2
smerdy <joshua.pham@gmail.com>
2016-12-11 21:43:01 -0600

    validated against training set and got 85% accuracy. need to validate against some test

--- tfidf_textrank.py
+++ tfidf_textrank.py
@@ -1,5 +1,10 @@
 import re
 from collections import defaultdict
+import nltk
+def grouped(iterable, n):
+    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
+    return zip(*[iter(iterable)]*n)
+
 
 with open('textrank_hotels_output.txt') as inf:
 	inf.readline()
@@ -14,7 +19,7 @@ with open('textrank_hotels_output.txt') as inf:
 
 		# is category title
 		if "***" in line:
-			catpat = re.compile('\w+')
+			catpat = re.compile('\w+') # re.compile('\w+#\w+')
 			curCategory = catpat.findall(line)
 			if curCategory:
 			 	curCategory = curCategory[0]
@@ -32,16 +37,46 @@ with open('textrank_hotels_output.txt') as inf:
 	for key, el in invIndex.items():
 		if '' in el:
 			el.remove('')
-		print(key)
-		print('\t', el)
+		# print(key)
+		# print('\t', el)
 
 
-		if len(el) > 2:
+		if len(el) > 1:
 			for cat in el:
 				category_bins[cat].remove(key)
 		del key
 
-	for cat, tokens in category_bins.items():
-		print(cat)
-		print('\t', list(tokens))
+	# for cat, tokens in category_bins.items():
+	# 	print(cat)
+	# 	print('\t', len(list(tokens)))
+
+	## Test against the original
+	
+	right = 0
+	wrong = 0
+	totalreviews = 0
+	found = False
+
+	with open('ABSA15_Hotels_parsed.txt') as testfile:
+		for sentence, category in grouped(iter(testfile), 2):
+			testcat = category.strip().split('#')[0].strip()
+			testsentence = sentence.strip()
+
+			if not testcat:
+				continue
+
+			for word in nltk.word_tokenize(testsentence):
+				if word in invIndex:
+					if testcat in invIndex[word]: # is this test category the one indexed category?
+						right += 1
+						found = True
+						break
+			if not found:
+				wrong += 1
+
+			totalreviews += 1
+			found = False
+
+	print("Accuracy: ", right / totalreviews)
+	print("Wrong: ", wrong / totalreviews)
 
